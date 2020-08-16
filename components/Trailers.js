3ced05ee17a99452fbe15ce11b5f03f39719de7d
imnotharsha <harshadodderi@gmail.com>
2020-08-16 13:05:34 +0530

    Updates

--- components/Trailers.js
+++
@@ -1,9 +0,0 @@
-const Trailers = {
-  season1: 'HhesaQXLuRY',
-  season2: 'I-8914DuyhY',
-  season3: 'ZK2IQ3LbLYk',
-  season4: 'WcUmzPcQEJo',
-  season5: '3oFofYisAko',
-  all: 'lrcqbavlbyQ',
-};
-export default Trailers;
